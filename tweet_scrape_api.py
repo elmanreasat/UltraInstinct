c3b2f451a8385c94f45a37bb3d86e68dcb9c22c8
BaninAbrar <baninabrar98@gmail.com>
2018-03-22 02:22:56 -0400

    Fixed twitter post extraction api

---
+++ tweet_scrape_api.py
@@ -0,0 +1,73 @@
+import tweepy #https://github.com/tweepy/tweepy
+import json
+from flask import Flask, render_template, request
+import tweet
+
+#Twitter API credentials
+consumer_key = "[]"
+consumer_secret = "[]"
+access_key = "[]"
+access_secret = "[]"
+
+def get_all_tweets(screen_name):
+
+    #Twitter only allows access to a users most recent 3240 tweets with this method
+
+    #authorize twitter, initialize tweepy
+    auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_key, access_secret)
+    api = tweepy.API(auth)
+
+    #initialize a list to hold all the tweepy Tweets
+    alltweets = []
+
+    #make initial request for most recent tweets (200 is the maximum allowed count)
+    new_tweets = api.user_timeline(screen_name = screen_name,count=200)
+
+    #save most recent tweets
+    alltweets.extend(new_tweets)
+
+    #save the id of the oldest tweet less one
+    oldest = alltweets[-1].id - 1
+
+    #keep grabbing tweets until there are no tweets left to grab
+    while len(new_tweets) > 0:
+
+        #all subsiquent requests use the max_id param to prevent duplicates
+        new_tweets = api.user_timeline(screen_name = screen_name,count=200,max_id=oldest)
+
+        #save most recent tweets
+        alltweets.extend(new_tweets)
+
+        #update the id of the oldest tweet less one
+        oldest = alltweets[-1].id - 1
+
+        print ("...%s tweets downloaded so far" % (len(alltweets)))
+
+    #write tweet objects to JSON
+    file = open('tweet.json', 'wb')
+    print ("Writing tweet objects to JSON please wait...")
+    for status in alltweets:
+        json.dump(status._json,file,sort_keys = True,indent = 4)
+
+
+
+    #close the file
+    print ("Done")
+    file.close()
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('test.html')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+    username = request.form['username']
+    get_all_tweets(username)
+    return 'Success <br/> <a href="/">Back Home</a>'
+
+if __name__ == '__main__':
+	app.run(host = '0.0.0.0', port = 3000)
+
